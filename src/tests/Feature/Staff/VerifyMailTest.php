11119d16769d249f73862b1b1155f43e2ac84d91
sumire sato <sumire.8007@gmail.com>
2025-06-07 11:43:02 +0900

    Update VerifyMailTest

--- src/tests/Feature/Staff/VerifyMailTest.php
+++ src/tests/Feature/Staff/VerifyMailTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Staff;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Tests\TestCase;
 use App\Models\User;
@@ -18,41 +19,46 @@ class VerifyMailTest extends TestCase
      * @return void
      */
     use RefreshDatabase;
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->user = User::create([
-            'name' => 'テスト太郎',
-            'email' => 'test123@example.com',
-            'password' => bcrypt('password123'),
-        ]);
-        Carbon::setTestNow(Carbon::create(2025, 5, 1, 12, 00, 0));
-    }
     //会員登録後、認証メールが送信される
     public function testSendEmail()
     {
-        Mail::fake();
-        $response = $this->post('register', [
+        Notification::fake();
+
+        $response = $this->post('/register', [
             'name' => 'テスト太郎',
             'email' => 'test123@example.com',
             'password' => 'password123',
             'password_confirmation' => 'password123',
-            'email_verified_at' => '2025-05-01 09:00:00',
         ]);
         $user = User::where('email', 'test123@example.com')->first();
-        Mail::assertQueued(VerifyEmail::class, function ($mail) use ($user) {
-            return $mail->hasTo($user->email);
-        });
+        $this->assertDatabaseHas('users', ['email' => 'test123@example.com']);
 
+        Notification::assertSentTo(
+            [$user],
+            VerifyEmail::class
+        );
     }
     //メール認証誘導画面で「認証はこちらから」ボタンを押下するとメール認証サイトに遷移する
     public function testTransitionSite()
     {
-
+        $response = $this->post('/register', [
+            'name' => 'テスト太郎',
+            'email' => 'test123@example.com',
+            'password' => 'password123',
+            'password_confirmation' => 'password123',
+        ]);
+        $user = User::where('email', 'test123@example.com')->first();
+        $this->assertDatabaseHas('users', ['email' => 'test123@example.com']);
+        $response = $this->actingAs($user)->get('/email/verify');
+        //認証動線画面が表示されているか
+        $response->assertSee('認証はこちらから');
+        //認証はこちらからのURLを押す(アクセス)
+        $this->get('http://localhost:8025/');
+        $response->assertStatus(200);
     }
     //メール認証サイトのメール認証を完了すると、勤怠画面に遷移する
-    public function testVerifiedEmail()
-    {
+    // public function testVerifiedEmail()
+    // {
 
-    }
+    // }
 }
